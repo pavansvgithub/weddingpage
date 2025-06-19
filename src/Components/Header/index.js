a4eae6ae4661e982d430a3f594cd60c93d572f20
Venkata Pavan Kumar <124552905+pavansvgithub@users.noreply.github.com>
2025-06-19 21:12:24 +0530

    Update index.js

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,19 +1,45 @@
-import "./index.css"
+import { NavLink } from "react-router-dom";
+import "./index.css";
 
-const Header=()=>{
-    return(
-      <div className="header-div px-5 mx-5 d-flex flex-row justify-content-start align-items-center bg-warnig">
-        <h1 className="sail-font primary-color header-h1">Wedding</h1>
-        <div className="jost-font primary-color navitem-div">
-            <a className="m-4 primary-color header-text" href="/">HOME</a>
-            <a className="m-4 primary-color header-text" href="/">SCHEDULES</a>
-            <a className="m-4 primary-color header-text"  href="/">RSVP</a>
-            <a className="m-4 primary-color header-text"  href="/">GALLERY</a>
-            <a className="m-4 primary-color header-text" href="/">GIFT REGISTRY</a>
-        </div>
+const Header = () => {
+  return (
+    <div className="header-div px-5 mx-5 d-flex flex-row justify-content-start align-items-center bg-warning">
+      <h1 className="sail-font primary-color header-h1">Wedding</h1>
+
+      <div className="jost-font primary-color navitem-div">
+        <NavLink
+          className="m-4 primary-color header-text"
+          to="/"
+        >
+          HOME
+        </NavLink>
+        <NavLink
+          className="m-4 primary-color header-text"
+          to="/schedule"
+        >
+          SCHEDULES
+        </NavLink>
+        <NavLink
+          className="m-4 primary-color header-text"
+          to="/rsvp"
+        >
+          RSVP
+        </NavLink>
+        <NavLink
+          className="m-4 primary-color header-text"
+          to="/gallery"
+        >
+          GALLERY
+        </NavLink>
+        <NavLink
+          className="m-4 primary-color header-text"
+          to="/gift-registry"
+        >
+          GIFT REGISTRY
+        </NavLink>
       </div>
-    )
-    
-}
+    </div>
+  );
+};
 
-export default Header
+export default Header;
