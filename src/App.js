e3d977977d337d0675e4cf71645b44db64cbe9c9
Venkata Pavan Kumar <124552905+pavansvgithub@users.noreply.github.com>
2025-06-19 23:18:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Home from './Components/Home';
-import Schedules from './Components/Schedules';
-import RSVP from './Components/RSVP';
-import Whenandwhere from './Components/when&where';
 import BookComponent from './Components/BookComponent';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import RSVP from './Components/RSVP';
+import Schedules from './Components/Schedules';
+import Whenandwhere from './Components/when&where';
 
-const App = () => {
-  return (
-    <Router>
-      <div className="main-div p-5">
-        <Header />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/schedule" element={<Schedules />} />
-          <Route path="/rsvp" element={<RSVP />} />
-          <Route path="/whenandwhere" element={<Whenandwhere />} />
-          <Route path="/book" element={<BookComponent />} />
-        </Routes>
-
-        <Footer />
-      </div>
-    </Router>
-  );
-};
+const App=()=>{
+  return(
+    <div className="main-div p-5">
+      <Header/>
+      <Home/>
+      <Schedules/>
+      <RSVP/>
+      <Whenandwhere/>
+      <BookComponent/>
+      <Footer/>
+    </div>
+  )
+}
 
 export default App;
